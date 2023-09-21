81cb170b20032d51ed57aa6c9f7d6ff809299672
l33keunha <l33keunha@gmail.com>
2023-09-21 21:52:45 +0900

    useState 함수형 컴포넌트에서 상태를 관리할 수 있음

--- react-native-component/src/App.js
+++ react-native-component/src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Text, View, Button } from 'react-native';
-import MyButton from './components/MyButton';
+import Counter from './components/Counter';
 
 const App = () => {
   return (
@@ -12,10 +12,7 @@ const App = () => {
         justifyContent: 'center',
       }}
     >
-      <Text style={{fontSize: 30, marginBottom: 10}}>Props</Text>
-      <MyButton title="Button" onPress={() => alert('props')}/> 
-      <MyButton title="Button" onPress={() => alert('children')}> Children Props</MyButton>
-      <MyButton onPress={() => alert('default')}/> 
+      <Counter/>
     </View>
   );
 };
