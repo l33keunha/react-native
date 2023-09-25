4d95745feb136bfd07404a48ba1e84cac70ace60
l33keunha <l33keunha@gmail.com>
2023-09-25 20:51:51 +0900

    클래스 스타일링 - 스타일 객체 사용, 인라인 스타일과 혼용 방식

--- react-native-style/src/App.js
+++ react-native-style/src/App.js
@@ -1,38 +1,32 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { StyleSheet,View, Text } from 'react-native';
 
 const App = () => {
     return (
-        <View
-            style={{
-                flex:1,
-                backgroundColor: '#fff',
-                alignItems: 'center',
-                justifyContent: 'center',
-            }}
-        >
-            <Text
-                style={{
-                    padding: 10,
-                    fontSize: 26,
-                    fontWeight: '600',
-                    color: 'black',
-                }}
-            >
-                Inline Styling - Text
-            </Text>
-            <Text
-                style={{
-                    padding: 10,
-                    fontSize: 26,
-                    fontWeight: '400',
-                    color: 'red',
-                }}
-            >
-                Inline Styling - Error
-            </Text>
+        <View style={styles.container}>
+           <Text style={[styles.text, {color: 'green'}]}>Inline Styling - Text</Text>
+           <Text style={[styles.text, styles.error]}>Inline Styling - Error</Text>
         </View>
-    )
-}
+    );
+};
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'cneter',
+    },
+    text: {
+        padding: 10,
+        fontSize: 26,
+        fontWeight: '600',
+        color: 'black',
+    },
+    error: {
+        fontWeight: '400',
+        color: 'red',
+    }
+})
 
 export default App;
