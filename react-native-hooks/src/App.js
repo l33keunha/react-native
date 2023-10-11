ddb31dafcd81723655be2ee6ac1baf2b85853d9c
l33keunha <l33keunha@gmail.com>
2023-10-11 22:13:16 +0900

    useEffect

--- react-native-hooks/src/App.js
+++ react-native-hooks/src/App.js
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, {useState} from 'react';
 import styled from 'styled-components/native';
-import Counter from './components/Counter';
+import Form from './components/Form';
+import Button from './components/Button'
 
 const Container = styled.View`
     flex:1;
@@ -10,9 +11,15 @@ const Container = styled.View`
 `;
 
 const App = () => {
+    const [isVisible, setIsVisible] = useState(true);
+
     return (
         <Container>
-            <Counter/>
+            <Button
+                title={isVisible ? 'Hide' : 'Show'}
+                onPress={() => setIsVisible(prev => !prev)}
+            />
+            {isVisible && <Form/>}
         </Container>
     )
 };
