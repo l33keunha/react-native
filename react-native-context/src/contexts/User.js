6382b92e3a25eea1df6182714cc06c2918f2fc98
l33keunha <l33keunha@gmail.com>
2023-10-28 10:21:21 +0900

    context

--- react-native-context/src/contexts/User.js
+++ react-native-context/src/contexts/User.js
@@ -1,5 +1,18 @@
-import { createContext } from "react";
+import { createContext, useState } from "react";
 
-const UserContext = createContext({ name: 'keunha Lee'});
+const UserContext = createContext({ 
+    user: {name:''},
+    dispatch: () => {},
+});
 
+const UserProvider = ({ children }) => {
+    const [name,setName] = useState('keunha Lee');
+
+    const value = { user: {name}, dispatch: setName };
+    return <UserContext.Provider value={value}>{children}</UserContext.Provider>
+};
+
+const UserConsumer = UserContext.Consumer;
+
+export { UserProvider, UserConsumer };
 export default UserContext;
