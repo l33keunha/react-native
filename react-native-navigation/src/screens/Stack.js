54bdb1ce8add0dda6ba7ade62eb7cd1612f5ed3d
l33keunha <l33keunha@gmail.com>
2023-12-12 15:24:26 +0900

    헤더 수정하기

--- react-native-navigation/src/screens/Stack.js
+++ react-native-navigation/src/screens/Stack.js
@@ -3,6 +3,8 @@ import { createStackNavigator } from "@react-navigation/stack";
 import Home from "./Home";
 import List from "./List";
 import Item from "./Item";
+import { MaterialCommunityIcons } from '@expo/vector-icons';
+import { Platform } from 'react-native';
 
 const Stack = createStackNavigator();
 
@@ -10,11 +12,52 @@ const StackNavigation = () => {
     return (
             <Stack.Navigator 
                 initialRouteName="Home"
-                screenOptions={{ cardStyle: { backgroundColor: '#ffffff' } }}
+                screenOptions={{ 
+                    cardStyle: { backgroundColor: '#ffffff' },
+                    headerStyle: {
+                        height: 110,
+                        backgroundColor: '#95a5a6',
+                        borderBottomWidth: 5,
+                        borderBottomColor: '#34495e'
+                    },
+                    headerTitleStyle: { color: '#ffffff', fontSize: 24 },
+                    headerTitleAlign: 'center',
+                    headerTitle: ({style}) => (
+                        <MaterialCommunityIcons name="react" style={style}/>
+                    ),
+                }}
             >
-                <Stack.Screen name = "Home" component={Home}/>
-                <Stack.Screen name = "List" component={List}/>
-                <Stack.Screen name = "Item" component={Item}/>
+                <Stack.Screen 
+                    name = "Home" 
+                    component={Home}
+                    options={{ headerShown: false }}
+                />
+                <Stack.Screen 
+                    name = "List" 
+                    component={List}
+                    options={{ 
+                        headerTitle: 'List Screen',
+                        headerBackTitleVisible: true,
+                        headerBackTitle: 'Prev',
+                        headerTitleStyle: { fontSize: 24 },
+                        headerTintColor: '#e74c3c',
+                        headerBackImage: ({ tintColor }) => {
+                            const style = {
+                                marginRight: 5, 
+                                marginLeft: Platform.OS === 'ios' ? 11:0,
+                            };
+                            return (
+                                <MaterialCommunityIcons
+                                    name="keyboard-backspace"
+                                    size={30}
+                                    color={tintColor}
+                                    style={style}
+                                />
+                            )
+                        }
+                    }}
+                />
+                <Stack.Screen name = "Detail" component={Item}/>
             </Stack.Navigator>
     )
 }
