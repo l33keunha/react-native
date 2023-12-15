320ed4e82a2697c3fe1d8708b0faef81b7a9e6ad
l33keunha <l33keunha@gmail.com>
2023-12-15 16:08:08 +0900

    탭 바 수정하기

--- react-native-navigation/src/screens/TabScreens.js
+++ react-native-navigation/src/screens/TabScreens.js
@@ -5,10 +5,12 @@ const Container = styled.View`
     flex: 1;
     justify-content: center;
     align-items: center;
+    background-color: #54b7f9;
 `;
 
 const StyledText = styled.Text`
     font-size: 30px;
+    color: #ffffff;
 `;
 
 export const Mail = () => {
